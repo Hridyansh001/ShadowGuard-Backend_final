bfd1e7b4f32e461e11992a6f23059073e9169de5
Hridyansh <deathbymaths@gmail.com>
2026-06-26 20:54:16 +0530

    added file scanning using tika and tested on through backend

---
+++ src/main/java/com/shadowguard/controller/FileScanController.java
@@ -0,0 +1,57 @@
+package com.shadowguard.controller;
+
+import com.shadowguard.model.Alert;
+import com.shadowguard.model.ScanResult;
+import com.shadowguard.service.AlertRepository;
+import com.shadowguard.service.FileScannerService;
+import com.shadowguard.service.Masking;
+import com.shadowguard.service.SensitiveDataClassifier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class FileScanController {
+
+    @Autowired
+    private SensitiveDataClassifier classifier;
+    @Autowired
+    private FileScannerService fileScannerService;
+    @Autowired
+    private Masking masking;
+    @Autowired
+    private AlertRepository alertRepository;
+
+    @PostMapping("/scan/file")
+    public ScanResult scanfile(@RequestParam("file")MultipartFile file) throws Exception
+    {
+        if(file.isEmpty())
+        {
+            throw new RuntimeException("no file uploaded");
+        }
+        String text = fileScannerService.extractText(file);
+        SensitiveDataClassifier.ClassificationResult result= classifier.classify(text);
+
+        String maskedText = masking.masktext(text);
+
+       if( !result.getVerdict().equals("SAFE")) {
+           Alert alert = new Alert(maskedText, result.getScore(), result.getVerdict(), result.getReasons());
+           alertRepository.save(alert);
+       }
+        return  new ScanResult(
+                maskedText,
+
+                result.getScore(),
+
+                result.getVerdict(),
+
+                result.getReasons(),
+
+                maskedText
+        );
+
+    }
+}
--- src/main/java/com/shadowguard/service/FileScannerService.java
+++ src/main/java/com/shadowguard/service/FileScannerService.java
@@ -1,4 +1,19 @@
 package com.shadowguard.service;
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-public class FileScannerService {
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class FileScannerService
+{
+    private final Tika tika = new Tika();
+
+    public String extractText(MultipartFile file) throws Exception
+    {
+        return tika.parseToString(file.getInputStream());
+    }
 }
